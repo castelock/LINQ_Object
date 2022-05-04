00dfc2dcb34939cc2b787effe96d6dc02d9f4627
juan jesus ojeda castelo <joc028@ual.es>
2022-05-04 17:48:08 +0200

    Creation of the project Cross Join.

---
+++ Cross_Join/Program.cs
@@ -0,0 +1,14 @@
+﻿using Cross_Join;
+
+var CrossJoinResult = from employee in Student.GetAllStudnets()
+                      from subject in Subject.GetAllSubjects()
+                      select new
+                      {
+                          Name = employee.Name,
+                          SubjectName = subject.SubjectName
+                      };
+foreach (var item in CrossJoinResult)
+{
+    Console.WriteLine($"Name : {item.Name}, Subject: {item.SubjectName}");
+}
+Console.ReadLine();
