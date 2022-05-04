aeb3930c257fcc1230a49df77b8e630d8d65309b
juan jesus ojeda castelo <joc028@ual.es>
2022-05-04 22:53:17 +0200

    Creation of the project ZIP.

---
+++ ZIP/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] numbersSequence = { 10, 20, 30, 40, 50 };
+string[] wordsSequence = { "Ten", "Twenty", "Thirty", "Fourty" };
+var resultSequence = numbersSequence.Zip(wordsSequence, (first, second) => first + " - " + second);
+foreach (var item in resultSequence)
+{
+    Console.WriteLine(item);
+}
+Console.ReadKey();
